7c18ba545183088c4a859a189614bdde7188c9ee
Javier Delgado <Javierd299@gmail.com>
2017-03-27 13:02:30 +0200

    Comentarios P4

--- EnunciadoP4/Vectores.asm
+++ EnunciadoP4/Vectores.asm
@@ -1,25 +1,49 @@
 ######################################################################
 ## Fichero: Vectores.asm
-## Descripción: Programa que...
-## Fecha última modificación: 
-
-## Autores: 
+## Descripción: Programa que guarda en un Array C el resultado de A+4B
+## Fecha última modificación: 2017-03-15
+## Autores: Javier Delgado del Cerro (2017), Javier López Cano (2017) 
+## Pareja: 4
 ## Asignatura: E.C. 1º grado
-## Grupo de Prácticas:
-## Grupo de Teoría:
+## Grupo de Prácticas: 2101
+## Grupo de Teoría: 210
 ## Práctica: 4
 ## Ejercicio: 1
 ######################################################################
 
-.text
+#############################
+# Equivalente en C
+# if (a < b) 
+#	c = b;
+# else
+#	c = a;
+# while (1);
+#############################
 
-main:		
+.text 	
+main:	
+	add $s0, $zero, $zero   #Inicializamos la vriable i a 0
+	lw $t0, N 				#Cargar en registro $t0 la variable N
+	sll $t0, $t0, 2 		#Multiplicamos N por 4 para poder compararlo con i, que aumenta de 4 en 4
+loop: 	
+	slt $t1, $s0, $t0  		#$t1 será 1 si i<N
+	beq $t1, $zero, fin 	#Si $t5 es 0, entonces no se cumple i<N, finalizamos el bucle
+	lw $t2, A($s0)			#Cargamos A[i] en $t2
+	lw $t3, B($s0)			#Cargamos B[i] en $t3
+	sll $t3, $t3, 2 		#Multiplicamos B[i] por 4
+	add $t4, $t2, $t3		#Sumamos A[i]+4*B[i]
+	sw $t4, C($s0)			#Escribimos el resultado en C[i]
+	addi $s0, $s0, 4 		#Sumamos de 4 en 4 para que corresponda a la siguiente posicion del array.
+	j loop 					#Salto al principio del bucle de nuevo
+fin: j fin
+	
+	
+.data  						#Comienzo de seccion de datos de usuario
+.align 2 					#Direccion alineada a palabra (multiplo de 2^2=4), no hace falta salvo que metamos a mano en .data una dirección que no sea múltiplo de 4
+A: 2, 2, 4, 6, 5, 6, 7, 8, 9, 10  #Variable A, ocupa una palabra. También se podría poner a: .word 5, pero es redundante porque por defecto ocupa una palabra
+B: -1, -5, 4, 10, 1, -2, 5, 10, -10, 0  #Variable B
+C: .space 40  							#Variable C
+N: 10									#Variable N, número de datos del array
+# Se podría haber puesto a: .word 5, 6, 0 pero entonces no podríamos hacer lw $t1, b. Habría que hacer lw $t1, a($t7) metiendo previamente un 4 en $t7
 
-
-
-.data
-A:
-B:
-C:
-N: 
 
--- EnunciadoP4/alcance.asm
+++ EnunciadoP4/alcance.asm
@@ -1,23 +1,43 @@
 ######################################################################
 ## Fichero: Alcance.asm
-## Descripción: Programa que...
-## Fecha última modificación:
-
-## Autores: 
+## Descripción: Programa que calcula el alcance de un tiro parabólico con V0=50m/s
+## Fecha última modificación: 2017-03-22
+## Autores: Javier Delgado del Cerro (2017), Javier López Cano (2017) 
+## Pareja: 4
 ## Asignatura: E.C. 1º grado
-## Grupo de Prácticas:
-## Grupo de Teoría:
+## Grupo de Prácticas: 2101
+## Grupo de Teoría: 210
 ## Práctica: 4
 ## Ejercicio: 2
 ######################################################################
 
 .text
 
-main:		
+main:	
+	addi $sp, $zero, 0x4000		#Inicializamos la pila
+	addi $sp, $sp, -4			#Hacemos push
+	lw $s0, angulo				#Leemos el valor del ángulo
+	sw $s0, 0($sp)				#Metemos el ángulo en la pila
+	jal calculaAlcance			#Llamamos a la funcion 
+	lw $s1, 0($sp)				#Hacemos pop de la pila y guardamos el resultado en $s1
+	addi $sp, $sp, 4		
+	sw $s1, resultado($zero) 	#Guardamos el resultado en la dirección de memoria resultado
+fin:	j fin					#Finalizamos el programa
 		
-calculaAlcance:
+calculaAlcance:	
+	lw $t0, 0($sp)				#Hacemos pop de la pila y guardamos el resultrado en $t0
+	addi $sp, $sp, 4			#En este caso, como hacemos un pop y un push, los dos addi se podrían omitir
+	sll $t0, $t0, 2				#Multiplicamos por 4	
+	lw $t1, lut($t0)			#Leemos el dato del angulo del array
+	addi $sp, $sp, -4			#Hacemos push del dato en la pila
+	sw $t1, 0($sp)	 
+	jr $ra
+
 
 
 .data
-lut: .word 0, 9, 17, 26, 35, 43, 52, 60, 69, 77, 86, 94, 102, 110, 117, 125, 132, 140, 147, 154, 161, 167, 174, 180, 186, 192, 197, 202, 207, 212, 217, 221, 225, 228, 232, 235, 238, 240, 243, 245, 246 # Faltan los valores correspondientes a los ángulos 41, 42, 43, 44 y 45 grados.
+resultado: 0
+angulo: 15
+#Array con los valores del alcance según los ángulos de 0 a 45
+lut: .word 0, 9, 17, 26, 35, 43, 52, 60, 69, 77, 86, 94, 102, 110, 117, 125, 132, 140, 147, 154, 161, 167, 174, 180, 186, 192, 197, 202, 207, 212, 217, 221, 225, 228, 232, 235, 238, 240, 243, 245, 246, 248, 249, 249, 250, 250 
 
