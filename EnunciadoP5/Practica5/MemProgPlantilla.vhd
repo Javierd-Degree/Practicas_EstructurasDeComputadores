b2168da7cb6d777036c0e7c0fc011bcca4fbcc68
Javier Delgado <Javierd299@gmail.com>
2017-04-05 10:44:32 +0200

    Practica 5

---
+++ EnunciadoP5/Practica5/MemProgPlantilla.vhd
@@ -0,0 +1,58 @@
+----------------------------------------------------------------------
+-- Fichero: MemProgPlantilla.vhd
+-- Descripción: Instrucciones de vectores.asm en código máquina
+-- Fecha última modificación: 2017-03-29
+
+-- Autores: Javier Delgado del Cerro y Javier López Cano
+-- Asignatura: EC 1º grado
+-- Grupo de Prácticas: 2101
+-- Grupo de Teoría: 210
+-- Práctica: 5
+-- Ejercicio: 2
+----------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+use IEEE.std_LOGIC_arith.ALL;
+use IEEE.std_logic_unsigned.ALL;
+
+entity MemProgPlantilla is
+	port (
+		MemProgAddr : in std_logic_vector(31 downto 0); -- Dirección para la memoria de programa
+		MemProgData : out std_logic_vector(31 downto 0) -- Código de operación
+	);
+end MemProgPlantilla;
+
+architecture Simple of MemProgPlantilla is
+
+begin
+
+	LecturaMemProg: process(MemProgAddr)
+	begin
+		-- La memoria devuelve un valor para cada dirección.
+		-- Estos valores son los códigos de programa de cada instrucción,
+		-- estando situado cada uno en su dirección.
+		case MemProgAddr is
+			when x"00000000" => MemProgData <= x"00008020"; 
+			when x"00000004" => MemProgData <= x"8C082000"; 
+			when x"00000008" => MemProgData <= x"01084020"; 
+			when x"0000000C" => MemProgData <= x"01084020"; 
+			when x"00000010" => MemProgData <= x"0208482A"; 
+			when x"00000014" => MemProgData <= x"11200008"; 
+			when x"00000018" => MemProgData <= x"8e0a2004"; 
+			when x"0000001C" => MemProgData <= x"8e0b201c"; 
+			when x"00000020" => MemProgData <= x"016b5820"; 
+			when x"00000024" => MemProgData <= x"016b5820"; 
+			when x"00000028" => MemProgData <= x"014b6020"; 
+			when x"0000002C" => MemProgData <= x"ae0c2034"; 
+			when x"00000030" => MemProgData <= x"22100004"; 
+			when x"00000034" => MemProgData <= x"08000004"; 
+			when x"00000038" => MemProgData <= x"0800000e"; 	
+			
+			when others => MemProgData <= X"00000000"; -- Resto de memoria vacía
+		end case;
+	end process LecturaMemProg;
+
+end Simple;
+
---
+++ EnunciadoP5/Vectores.asm
@@ -0,0 +1,51 @@
+######################################################################
+## Fichero: Vectores.asm
+## Descripción: Programa que guarda en un Array C el resultado de A+4B
+## Fecha última modificación: 2017-03-15
+## Autores: Javier Delgado del Cerro (2017), Javier López Cano (2017) 
+## Pareja: 4
+## Asignatura: E.C. 1º grado
+## Grupo de Prácticas: 2101
+## Grupo de Teoría: 210
+## Práctica: 4
+## Ejercicio: 1
+######################################################################
+
+#############################
+# Equivalente en C
+# if (a < b) 
+#	c = b;
+# else
+#	c = a;
+# while (1);
+#############################
+
+.text 	
+main:	
+	add $s0, $zero, $zero   #Inicializamos la vriable i a 0
+	lw $t0, N 				#Cargar en registro $t0 la variable N
+	add $t0, $t0, $t0 		#Multiplicamos N por 2 sumandolo a si mismo 
+	add $t0, $t0, $t0		#Multiplicamos de nuevo por 2 para poder compararlo con i, que aumenta de 4 en 4
+loop: 	
+	slt $t1, $s0, $t0  		#$t1 será 1 si i<N
+	beq $t1, $zero, fin 	#Si $t5 es 0, entonces no se cumple i<N, finalizamos el bucle
+	lw $t2, A($s0)			#Cargamos A[i] en $t2
+	lw $t3, B($s0)			#Cargamos B[i] en $t3
+	add $t3, $t3, $t3 		#Multiplicamos B[i] por 2 sumandolo a si mismo
+	add $t3, $t3, $t3 		#Multiplicamos B[i] de nuevo por 2 para que sea múltiplo de 4
+	add $t4, $t2, $t3		#Sumamos A[i]+4*B[i]
+	sw $t4, C($s0)			#Escribimos el resultado en C[i]
+	addi $s0, $s0, 4 		#Sumamos de 4 en 4 para que corresponda a la siguiente posicion del array.
+	j loop 					#Salto al principio del bucle de nuevo
+fin: j fin
+	
+	
+.data  				#Comienzo de seccion de datos de usuario
+.align 2 			#Direccion alineada a palabra (multiplo de 2^2=4), no hace falta salvo que metamos a mano en .data una dirección que no sea múltiplo de 4
+N: 6				#Variable N, número de datos del array
+A: 2, 4, 6, 8, 10, 12 		 #Array A
+B: -1, -5, 4, 10, 1, -5		 #Array B
+C: .space 24  			#Array C, reservamos espacio
+									
+
+
